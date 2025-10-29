dfede776a582a02b2579d9293aa86151811c7f17
Esteban Peregrina <110783029+esteban-peregrina@users.noreply.github.com>
2025-10-29 14:31:21 +0100

    FEAT: Adds parse_arg_string() function
    Allow for correct parsing of the input string

--- TP4 - Microshell/inc/parser.h
+++ TP4 - Microshell/inc/parser.h
@@ -2,5 +2,6 @@
 #define PARSER_H
 
 int count_arguments(char buffer[]);
+char** parse_arg_string(char buffer[], int arg_count);
 
 #endif // PARSER_H
--- TP4 - Microshell/src/main.c
+++ TP4 - Microshell/src/main.c
@@ -3,16 +3,22 @@
 
 #include <stdio.h>
 
-#define MAX_ARGC
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char **argv) { // TODO -v verbose
+#define MAX_ARGC 128 // Default 128
+
+int main(int argc, char **argv) { // TODO -v verbose et usage
     char buffer[MAX_LENGTH];
     prompt(buffer);
     fprintf(stdout, "Prompt is: %s\n", buffer);
     int arg_count = count_arguments(buffer);
     printf("ARGC = %d\n", arg_count);
     
-    char** arg_values = NULL;
+    char** arg_values = parse_arg_string(buffer, arg_count);
+    for (int i = 0; i < arg_count; i++) printf("%s\n", arg_values[i]);
+    free(arg_values);
 
     return 0;
 }
