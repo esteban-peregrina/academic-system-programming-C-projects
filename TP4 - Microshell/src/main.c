1bb92a36a632087a7950a823135d8fc09660f096
Esteban Peregrina <110783029+esteban-peregrina@users.noreply.github.com>
2025-11-02 18:12:26 +0100

    REF: Rework unit command extraction to use strtok() and optimise code
    Remove need for strdup()

--- TP4 - Microshell/src/main.c
+++ TP4 - Microshell/src/main.c
@@ -25,10 +25,7 @@ int exec_unit_command(unit_command_t* unit_command) {
     } else {
         //fprintf(stdout, "I'm the parent, my PID is %d and my child's PID is %d !\n", getpid(), pid);
         int status;
-        if(!unit_command->async) { 
-            waitpid(pid, &status, 0);
-            free(unit_command->token_array); // Seul l'un des deux doit libérer la mémoire //TODO gérer free si background
-        }
+        if(!unit_command->async) { waitpid(pid, &status, 0); }
     }
     return 0;
 }
@@ -51,39 +48,44 @@ int main(int argc, char **argv) { // TODO -v verbose et usage
             exit(EXIT_FAILURE);
         }
 
-        // On remplit le tableau
-        char* start = buffer;
-        int i = 0;
-        for (char* c = buffer; *c != '\0'; c++) {
-            if (*c == ';' || *c == '|' || *c == '>' || *c == '&') {
-                // Création d'une commande 
-                unit_command_t* unit_command = NULL;
-                unit_command = malloc(sizeof(unit_command_t));
-                if (unit_command == NULL) {
-                    fprintf(stderr, "Error: malloc failed (%s)\n", strerror(errno));
-                    exit(EXIT_FAILURE);
-                }
-                // Initialisations
-                unit_command->async = 0;
-                unit_command->separator = SEP_NONE;
-
-                // Affectations conditionnelles
-                if (*c == ';') {unit_command->separator = SEP_SEQUENCE; }
-                if (*c == '|') {unit_command->separator = SEP_PIPE; }
-                if (*c == '&') {unit_command->separator = SEP_BACKGROUND; unit_command->async = 1; }
-                if (*c == '>') {unit_command->separator = SEP_REDIRECT; } // TODO unit_command->outfile
-                *c = '\0';
-                unit_command->raw_command = strdup(start); // ALLOCATION DYNAMIQUE !
-                start = c + 1;
-                unit_cmd_array[i++] = unit_command;
+        // On ajoute les commandes unitaires
+        for (int i = 0; i < unit_cmd_count; i++) {
+            // Allocation
+            unit_command_t* unit_command = NULL;
+            unit_command = malloc(sizeof(unit_command_t));
+            if (unit_command == NULL) {
+                fprintf(stderr, "Error: malloc failed (%s)\n", strerror(errno));
+                exit(EXIT_FAILURE);
             }
+
+            // Initialisations
+            unit_command->async = 0;
+            unit_command->separator = SEP_NONE;
+            unit_command->raw_command = NULL;
+            
+            // Ajout dans le tableau de commandes unitaire
+            unit_cmd_array[i] = unit_command;
         }
-        // On ajoute la dernière
-        unit_command_t* last = malloc(sizeof(unit_command_t));
-        last->raw_command = strdup(start);
-        last->separator = SEP_NONE;
-        last->async = 0;
-        unit_cmd_array[i++] = last;
+
+        // On ajoute les séparateurs en reparcourant le buffer
+        int separator = 0;
+        for (char* c = buffer; *c != '\0'; c++) { 
+            if (*c == ';') {unit_cmd_array[separator++]->separator = SEP_SEQUENCE; }
+            else if (*c == '|') {unit_cmd_array[separator++]->separator = SEP_PIPE; }
+            else if (*c == '&') {unit_cmd_array[separator]->separator = SEP_BACKGROUND; unit_cmd_array[separator++]->async = 1; } // Ligne tricky haha
+            else if (*c == '>') {unit_cmd_array[separator++]->separator = SEP_REDIRECT; } // TODO unit_command->outfile
+        }
+        unit_cmd_array[separator]->separator = SEP_NONE; // La dernière commande
+
+        // On ajoute chaque séquence à sa commande unitaire
+        int c = 0;
+        const char * delimiter = ";|>&";
+        char* command = strtok(buffer, delimiter);
+        for (int i = 0; i < unit_cmd_count; i++) {
+            if (command == NULL) { break; }
+            unit_cmd_array[c++]->raw_command = command;
+            command = strtok(NULL, delimiter); // On passe à la commande suivante
+        }
 
         // On exécute tout ce qui est dans le tableau
         for (int i = 0; i < unit_cmd_count; i++) {
@@ -103,8 +105,9 @@ int main(int argc, char **argv) { // TODO -v verbose et usage
 
         // On libère
         for (int i = 0; i < unit_cmd_count; i++) {
-            free(unit_cmd_array[i]->raw_command);
+            free(unit_cmd_array[i]->token_array); // Even if background ?
             free(unit_cmd_array[i]);
+            // Pas besoin de free les raw_command, car ils pointent sur le buffer, fractionné par strtok()
         }
         free(unit_cmd_array);
     }
